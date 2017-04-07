dd6b83ed04802d05a1120f27c3a36e681300238c
Daronchic <daronchic@yandex.ru>
2017-04-07 14:56:37 +0300

    Added Player data in starting menu

--- Havoiskateli/StartingState.cpp
+++ Havoiskateli/StartingState.cpp
@@ -3,6 +3,22 @@
 #include "ResourceManager.h"
 #include "Globals.h"
 
+struct PlayerData {
+	std::string playerName;
+	int lifes, inventory;
+	sf::String description;
+
+	PlayerData() {}
+	PlayerData(int lifes, int inventory, const sf::String &description) :
+		lifes(lifes), inventory(inventory), description(description) {}
+	void setPlayerName(const std::string &name)
+	{
+		playerName = name;
+	}
+};
+
+static inline std::string convertToString(int value);
+
 StartingState::StartingState() : State()
 {
 }
@@ -38,11 +54,17 @@ void StartingState::load()
 		m_playerStands.emplace_back(new AnimatedObject(*ResourceManager::getInstance()->getImage(Images::menu_stand), 0, 0));
 	}
 	m_players.emplace_back(new Player(*ResourceManager::getInstance()->getImage(Images::player_1), 0, 0, 0, 0));
+	m_playerData.emplace_back(PlayerData(3, 3, L"description of orange yam yam. \nHe is as regular as his developer, \nvery weak, not really smart \nand very hungry, \nGood luck, dude xD"));
 	m_players.emplace_back(new Player(*ResourceManager::getInstance()->getImage(Images::player_2), 0, 0, 0, 0));
+	m_playerData.emplace_back(PlayerData(3, 4, L"description of dark yam yam. \nThis dude is really serious men, \nHe looks like night, like darkness! \nHis inventory is very spacious!"));
 	m_players.emplace_back(new Player(*ResourceManager::getInstance()->getImage(Images::player_3), 0, 0, 0, 0));
+	m_playerData.emplace_back(PlayerData(3, 3, L"description of green yam yam. \nDudes, this is your chance! \nAs you can see - he is green! \nLike grass or... green sky.. \nwhatever"));
 	m_players.emplace_back(new Player(*ResourceManager::getInstance()->getImage(Images::player_4), 0, 0, 0, 0));
+	m_playerData.emplace_back(PlayerData(3, 3, L"description of blue yam yam. \nIm not sure, but I think that guy \ncan swim or, actually he \ncould stay alive in water \nor else..."));
 	m_players.emplace_back(new Player(*ResourceManager::getInstance()->getImage(Images::player_1), 0, 0, 0, 0));
+	m_playerData.emplace_back(PlayerData(3, 3, L"description of ... yam yam?\n Who is this guy? I dont know, \nhe looks like regular, but \nhe just stole his sprite"));
 	m_players.emplace_back(new Player(*ResourceManager::getInstance()->getImage(Images::player_2), 0, 0, 0, 0));
+	m_playerData.emplace_back(PlayerData(6, 5, L"description of ... yam yam? \nit will be super kitten! \nReally! With pair of ears \nand, you know, as hungry \nas real cat!"));
 
 }
 
@@ -59,6 +81,11 @@ void StartingState::init()
 	m_descriptionRect = sf::RectangleShape(sf::Vector2f(m_window->getSize().x / 2, m_window->getSize().y / 2));
 	m_descriptionRect.setPosition(m_window->getSize().x / 2-m_descriptionRect.getSize().x / 2, m_window->getSize().y / 2);
 	m_descriptionRect.setFillColor(sf::Color(128, 109, 176, 217));
+
+	m_descriptionString = "Player name- %1\nHP- %2\nInventory capacity- %3 \n\nDescription- %4";
+	m_descriptionText.setFont(*ResourceManager::getInstance()->getFont(Fonts::menuFont));
+	m_descriptionText.setPosition(m_descriptionRect.getPosition().x + 10, m_descriptionRect.getPosition().y + 10);
+
 }
 
 void StartingState::handleInput(sf::Event & event)
@@ -69,9 +96,11 @@ void StartingState::handleInput(sf::Event & event)
 	}
 	if (event.type == sf::Event::KeyReleased && event.key.code == sf::Keyboard::Left) {
 		m_switched--;
+		m_descriptionString = "Player name- %1\nHP- %2\nInventory capacity- %3 \n\nDescription- %4";
 	}
 	else if (event.type == sf::Event::KeyReleased && event.key.code == sf::Keyboard::Right) {
 		m_switched++;
+		m_descriptionString = "Player name- %1\nHP- %2\nInventory capacity- %3 \n\nDescription- %4";
 	}
 }
 
@@ -83,6 +112,9 @@ void StartingState::update(float elapsed)
 		m_switched = 5;
 	for (int i = 0; i < m_playerStands.size(); i++)
 	{
+		m_playerStands[i]->update(elapsed);
+		m_players[i]->update(elapsed);
+		setPlayerData();
 		if (i == m_switched) {
 			m_playerStands[i]->changeState(true);
 			if (m_players[i]->getPosition().y > m_switchedY) {
@@ -101,8 +133,6 @@ void StartingState::update(float elapsed)
 				m_players[i]->stopVerticalMoving();
 			}
 		}
-		m_playerStands[i]->update(elapsed);
-		m_players[i]->update(elapsed);
 	}
 }
 
@@ -119,4 +149,21 @@ void StartingState::draw()
 		m_window->draw(*players);
 	}
 	m_window->draw(m_descriptionRect);
+	m_window->draw(m_descriptionText);
+}
+
+void StartingState::setPlayerData()
+{
+	m_descriptionString.replace("%1", m_playerData[m_switched].playerName);
+	m_descriptionString.replace("%2", convertToString(m_playerData[m_switched].lifes));
+	m_descriptionString.replace("%3", convertToString(m_playerData[m_switched].inventory));
+	m_descriptionString.replace("%4", m_playerData[m_switched].description);
+	m_descriptionText.setString(m_descriptionString);
+}
+
+static std::string convertToString(int value)
+{
+	std::stringstream strBuf;
+	strBuf << value;
+	return strBuf.str();
 }
--- Havoiskateli/StartingState.h
+++ Havoiskateli/StartingState.h
@@ -3,6 +3,8 @@
 #include "Player.h"
 #include "AnimatedObject.h"
 
+struct PlayerData;
+
 class StartingState :
 	public State
 {
@@ -16,13 +18,17 @@ public:
 	void init();
 	void handleInput(sf::Event &event);
 	void update(float elapsed);
+	void setPlayerData();
 	void draw();
 private:
 	int m_switched{ 3 };
 	std::vector<Player*> m_players;
 	std::vector<AnimatedObject*> m_playerStands;
+	std::vector<PlayerData> m_playerData;
 	sf::RectangleShape m_descriptionRect;
 	int m_switchedY;
 	MSprite m_bigStand;
 
+	sf::String m_descriptionString;
+	sf::Text m_descriptionText;
 };
